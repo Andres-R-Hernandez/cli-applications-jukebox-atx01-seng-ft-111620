67d18300b5ab4cf73e501ade848767f72091c4d3
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 20:38:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,30 @@
 # Add your code here
+def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.strip
+ 
+puts say_hello(users_name)
+
+
+def help
+  puts "I accept the following commands:\n
+  - help : displays this help message\n
+  - list : displays a list of songs you can play\n
+  - play : lets you choose a song to play\n
+  - exit : exits this program"
+end
+
+def list
+end
+
+def play
+end
+
+def exit_jukebox
+end
+
+def run
+end
