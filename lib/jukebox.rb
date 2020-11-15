ffa48803284ec213872e75d851c4cc6d96710c94
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 23:40:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,6 +12,7 @@ def list(songs)
     puts "#{index+1}. #{song}"
   end
 end
+=begin
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -23,6 +24,7 @@ songs = [
   "Harry Chapman - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+=end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
@@ -34,7 +36,7 @@ def play(songs)
     puts "Invalid input, please try again"
   end
 end
-play(songs)
+#play(songs)
 def exit_jukebox
   puts "Goodbye"
 end
