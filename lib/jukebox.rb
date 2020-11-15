022c91bd37df10c6042fd619dfa929fe092db836
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 21:41:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   if input.to_i < songs.length
-    puts "Playing #{songs[input.to_i + 1]}"
+    puts "Playing #{songs[input.to_i - 1]}"
   elsif songs.any?(input)
     stuff
   else
