d6cc886d2a774161deb9aa9136bbab11540dd1ce
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 21:39:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input.to_i < songs.length
+  if input < songs.length
     stuff
   elsif songs.any?(input)
     stuff
