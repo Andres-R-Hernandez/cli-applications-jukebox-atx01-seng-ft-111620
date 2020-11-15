9a6b199cb7b1f5ca774099f258d0f26632c113c5
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 23:27:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input.class == Integer && input < songs.size
+  if input.class == Integer
     puts "Playing #{songs[input - 1]}"
   elsif input.class == String && songs.any?(input)
     puts "Playing #{input}"
