f1efbe14f698a8333c3de1b70ac8f3d7be00ac8e
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 21:06:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,14 +1,4 @@
 # Add your code here
-def say_hello(name)
-  "Hi #{name}!"
-end
-
-puts "Enter your name:"
-users_name = gets.strip
-
-puts say_hello(users_name)
-
-
 def help
   puts "I accept the following commands:\n
         - help : displays this help message\n
