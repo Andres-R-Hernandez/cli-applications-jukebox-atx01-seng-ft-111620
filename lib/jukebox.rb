a552434562f83bd4278cade83cd4df9670fe1908
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 20:38:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,10 +11,10 @@ puts say_hello(users_name)
 
 def help
   puts "I accept the following commands:\n
-  - help : displays this help message\n
-  - list : displays a list of songs you can play\n
-  - play : lets you choose a song to play\n
-  - exit : exits this program"
+        - help : displays this help message\n
+        - list : displays a list of songs you can play\n
+        - play : lets you choose a song to play\n
+        - exit : exits this program"
 end
 
 def list
